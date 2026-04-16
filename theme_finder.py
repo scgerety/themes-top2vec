086ff4a46a2c1322f99b3a61b24afda9b42f51ec
Sean Gerety <71694412+scgerety@users.noreply.github.com>
2026-04-16 10:20:49 -0400

    Added theme_finder.py

---
+++ theme_finder.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import numpy as np
+from top2vec import Top2Vec
+
+filepath = os.path.abspath(os.path.dirname("theme_finder.ipynb"))
+analysis_folder = "Analysis"
+data_file = "PSI2025Evals.csv"
+comment_file = os.path.join(filepath, analysis_folder, data_file)
+
+df = pd.read_csv(comment_file, header=1, skiprows=[2])
+df = df.dropna(subset="Please provide any additional comments you would like to add here.")
+comments = df[["Please provide any additional comments you would like to add here.","session"]]
+comments.columns = ["comments","session"]
+
+docs = comments.comments.tolist()
+new_model = Top2Vec(docs, embedding_model="universal-sentence-encoder")
+
+print(new_model)

9561c1b0d1e62fb5c506434fe828fb2bd8cd7f8c
Sean Gerety <scgerety@gmail.com>
2026-04-16 14:40:44 -0400

    Added to gitignore, changed directory location for data in theme_finder.py

--- theme_finder.py
+++ theme_finder.py
@@ -3,9 +3,8 @@ import pandas as pd
 from top2vec import Top2Vec
 
 filepath = os.path.abspath(os.path.dirname("theme_finder.ipynb"))
-analysis_folder = "Analysis"
 data_file = "PSI2025Evals.csv"
-comment_file = os.path.join(filepath, analysis_folder, data_file)
+comment_file = os.path.join(filepath, data_file)
 
 df = pd.read_csv(comment_file, header=1, skiprows=[2])
 df = df.dropna(subset="Please provide any additional comments you would like to add here.")
